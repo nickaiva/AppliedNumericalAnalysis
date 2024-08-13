0321267cd7e232679cf20bb34a1429ea805f02f4
Nick Aiva <nickaiva@yahoo.gr>
2024-08-13 11:32:55 +0300

    Errors and relative errors added

--- chapter0/main.c
+++ chapter0/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 
+/* Example output
+
+Double Summary = 1.0000000000000006661338147750939
+ error = 0.000000000000000666133814775  relative Error = 0.000000000000000666133814775
+Float Summary = 0.9999907016754150390625000000000
+ error = 0.000009298324584960937500000  relative Error = 0.000009298324584960937500000
+
+*/
 void floatSum();
 void doubleSum();
 
+double actual = 1.0;
+float actualfloat = 1.0f;
 int main()
 {
     doubleSum();//Both ought to calculate 1.0
@@ -15,7 +26,9 @@ void floatSum(){
     for (int i =0 ; i< 1000; i++){
         sum += .001;
     }
-    printf("Float Summary = %1.31f \n",sum);
+    float Error = fabs(actualfloat - sum);
+    float relativeError = Error / actualfloat;
+    printf("Float Summary = %1.31f \n error = %5.27f  relative Error = %5.27f\n",sum, Error, relativeError);
 }
 
 void doubleSum(){
@@ -23,5 +36,7 @@ void doubleSum(){
     for (int i =0 ; i< 1000; i++){
         sum += .001;
     }
-    printf("Double Summary = %1.31f \n",sum);
+    double Error =  fabs( actual - sum);
+    double relativeError = Error / actual;
+    printf("Double Summary = %1.31f \n error = %5.27f  relative Error = %5.27f\n",sum, Error, relativeError);
 }
